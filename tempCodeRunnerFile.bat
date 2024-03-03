c35422936bbcaea02241c22edd37e0f212c46b36
Modig20 <160322947+Modig20@users.noreply.github.com>
2024-03-03 22:48:19 +0800

    Add files via upload

---
+++ tempCodeRunnerFile.bat
@@ -0,0 +1,18 @@
+<div class="blank"></div>
+
+    <div class="scontainer">
+
+      <div class="box">Vertebrates
+      <img src="ver.jpg">
+      </div>
+      <div class="box">Invertebrates
+      <img src="inver.jpg">
+      </div>
+      <div class="box">Other Phylum
+      <img src="other.jpg">
+      </div>
+    
+    </div>
+    
+</body>
+</html>
